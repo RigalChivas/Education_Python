1b6824b85811d5a355b6497bdb56dc4c3d9a9641
RigalChivas <anton.olegich@gmail.com>
2025-02-16 10:36:14 +0500

    слияние и прочее

--- Кортежы/cortej.py
+++ Кортежы/cortej.py
@@ -1,1 +1,28 @@
-print('hello world')
+# post_ids = (151, 245, 768, 245)
+# print(post_ids.count(245))  # 2
+# print(post_ids.count(151))  # 1
+
+
+
+# post_ids = (151, 245, 768, 245)
+# print(post_ids.index(245))  # 2
+# print(post_ids.index(768))  # 1
+
+
+
+# post_ids = (151, 245, 768, 245)
+
+# post_ids_list = list(post_ids)
+# post_ids_list.append(351)
+
+# print(post_ids_list)  # [151, 245, 768, 245, 351]
+
+# post_ids_tuple = tuple(post_ids_list)
+# print(post_ids_tuple)  # (151, 245, 768, 245, 351)
+
+my_nums = (10, 5, 100, 0)
+
+print(type(my_nums))  # <class 'tuple'>
+
+
+
