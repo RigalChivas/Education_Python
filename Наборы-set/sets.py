1d4a0718386beaeec0039f6d6c4f1cfcb4d539ef
RigalChivas <anton.olegich@gmail.com>
2025-02-17 20:53:27 +0500

    practice

---
+++ Наборы-set/sets.py
@@ -0,0 +1,10 @@
+# my_set = {'abc', 'd', 'f', 'y'}
+# other_set = {'a', 'f', 'd'}
+
+# print(my_set.intersection('abcd'))  # {'d', 'f'}
+#print(other_set.intersection(my_set))  # {'d', 'f'}
+
+my_set = {'abc', 'd', 'f', 'y'}
+other_set = {'a', 'f', 'd'}
+
+print(my_set.union(other_set))
