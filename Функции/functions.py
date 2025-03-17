1e90febbf4382d019327c16b29f345bc4bb096e7
RigalChivas <anton.olegich@gmail.com>
2025-03-17 20:38:30 +0500

    functions done

--- Функции/functions.py
+++ Функции/functions.py
@@ -13,13 +13,24 @@
 # print(new_person[ 'age'])
 # print(person_one['age'])
 
-def merge_list_to_dict(list_one, list_two):
-    zipped_seq = zip(list_one, list_two)
-    return dict(zipped_seq)
+# def merge_list_to_dict(list_one, list_two):
+#     zipped_seq = zip(list_one, list_two)
+#     return dict(zipped_seq)
 
 
-res_one = merge_list_to_dict([1, 2, 3], ['a', 'b', 'c'])
-print(res_one)
+# res_one = merge_list_to_dict([1, 2, 3], ['a', 'b', 'c'])
+# print(res_one)
 
 
-res_two = merge_list_to_dict([13], ['a', 'b', 'c', 'd'])
+# res_two = merge_list_to_dict(['abc'], [{}, True, 100])
+# print(res_two)
+
+# def sum_nums(*args):
+#     print(args)
+#     print(type(args))
+#     print(args[0])
+#     return sum(args)
+
+# print(sum_nums(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+
