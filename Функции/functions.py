7dfcc45d0b680a209f8ebb7791ae0dd96f156c24
RigalChivas <anton.olegich@gmail.com>
2025-03-18 22:21:12 +0500

    123

--- Функции/functions.py
+++ Функции/functions.py
@@ -13,13 +13,41 @@
 # print(new_person[ 'age'])
 # print(person_one['age'])
 
-def merge_list_to_dict(list_one, list_two):
-    zipped_seq = zip(list_one, list_two)
-    return dict(zipped_seq)
+# def merge_list_to_dict(list_one, list_two):
+#     zipped_seq = zip(list_one, list_two)
+#     return dict(zipped_seq)
 
 
-res_one = merge_list_to_dict([1, 2, 3], ['a', 'b', 'c'])
-print(res_one)
+# res_one = merge_list_to_dict([1, 2, 3], ['a', 'b', 'c'])
+# print(res_one)
 
 
-res_two = merge_list_to_dict([13], ['a', 'b', 'c', 'd'])
+# res_two = merge_list_to_dict([13], ['a', 'b', 'c', 'd'])
+
+# значение функции по умолчанию
+# def multi_by_factor(value, multiplier=1):
+#     return value * multiplier
+
+# print(multi_by_factor(5, 2))
+# print(multi_by_factor(5))
+
+# from datetime import date
+
+# def get_weekday():
+#     return date.today().strftime('%A')
+
+# def create_new_post(post, weelday=get_weekday()):
+#     post_copy = post.copy()
+#     post_copy['created_on_weekday'] = weelday
+#     return post_copy
+
+# initial_post = {
+#     'id':243,
+#     'author': 'Jack',
+#     'text': 'Hello, world!'
+# }
+
+# post_with_weekday = create_new_post(initial_post)
+
+# print(post_with_weekday)
+
