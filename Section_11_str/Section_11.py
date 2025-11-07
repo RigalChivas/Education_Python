ec0e8822714137829e2b4900f3e0f38239603c33
Anton <anton.olegich@gmail.com>
2025-11-07 09:54:22 +0500

    go

---
+++ Section_11_str/Section_11.py
@@ -0,0 +1,12 @@
+# ###long_str = """This is a long string 
+# that spans multiple lines."""
+
+# print(long_str)
+
+# print(type(long_str))
+# ###
+#---------------------_#------------------------
+my_name = "AntonioBanderas"
+print(len(my_name))  # Antonio
+print(my_name[0:7])  # Antonio
+print(my_name[7:])   # Banderas
